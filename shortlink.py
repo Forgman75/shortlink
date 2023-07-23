c12d6e3cecba34cf585b6c345ae4c22161ddaf36
Forgman75 <agolikov2020@gmail.com>
2023-07-23 22:18:48 +0300

    Added the is_bitlink function

--- shortlink.py
+++ shortlink.py
@@ -1,12 +1,24 @@
 import requests
 import sys
 from account import token
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
 
 base_url = 'https://api-ssl.bitly.com/v4/'
 
+def is_bitlink(token, input_url):
+    link = urlparse(input_url)
+    bitlink_path = "bitlinks/{lh}{lp}".format(lh=link.hostname, lp=link.path)
+    url = urljoin(base_url, bitlink_path)
+    bearer = "Bearer {t}".format(t=token)
+    headers = {
+        "Authorization": bearer 
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.ok
 
-def shorten_link(link):
+
+def shorten_link(token, link):
     url = "{b}shorten".format(b=base_url)
     bearer = "Bearer {t}".format(t=token)
     headers = {
@@ -19,8 +31,9 @@ def shorten_link(link):
     response.raise_for_status()
     return response.json()['link']
 
-def count_clicks(token, link):
-    clicks_path = "bitlinks/{l}/clicks".format(l=link)
+def count_clicks(token, input_url):
+    link = urlparse(input_url)
+    clicks_path = "bitlinks/{lh}{lp}/clicks".format(lh=link.hostname, lp=link.path)
     url = urljoin(base_url, clicks_path)
     bearer = "Bearer {t}".format(t=token)
     headers = {
@@ -36,11 +49,21 @@ def count_clicks(token, link):
     clicks_count = clicks_json[0].get('clicks')
     return clicks_count
 
+
+
 if __name__ == "__main__":
- link = input("Введите ссылку, которую нужно сократить:")
- try:  
-    bitlink = shorten_link(link)
- except requests.exceptions.HTTPError:
-    print("Вы ввели не корректную ссылку")
-    sys.exit()   
- print('Битлинк', bitlink)
+ user_input = input("Введите ссылку:")
+ if is_bitlink(token, user_input):
+    try:
+        c_count = count_clicks(token, user_input)
+        print("По вашей ссылке прошли {c} раз(а)".format(c=c_count))
+    except requests.exceptions.HTTPError:
+        print("Вы ввели не корректную ссылку")
+        sys.exit()
+ else:
+    try:  
+        bitlink = shorten_link(token, user_input)
+        print('Битлинк', bitlink)
+    except requests.exceptions.HTTPError:
+        print("Вы ввели не корректную ссылку")
+        sys.exit()
