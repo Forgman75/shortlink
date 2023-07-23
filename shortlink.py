dc64df541f21dd1c11c3b9add0ec62cc17b80d84
Forgman75 <agolikov2020@gmail.com>
2023-07-23 21:28:15 +0300

    Added the count_clicks function

--- shortlink.py
+++ shortlink.py
@@ -1,9 +1,13 @@
 import requests
 import sys
 from account import token
-url = 'https://api-ssl.bitly.com/v4/shorten'
+from urllib.parse import urljoin
+
+base_url = 'https://api-ssl.bitly.com/v4/'
+
+
 def shorten_link(link):
-    
+    url = "{b}shorten".format(b=base_url)
     bearer = "Bearer {t}".format(t=token)
     headers = {
         "Authorization": bearer 
@@ -15,11 +19,28 @@ def shorten_link(link):
     response.raise_for_status()
     return response.json()['link']
 
+def count_clicks(token, link):
+    clicks_path = "bitlinks/{l}/clicks".format(l=link)
+    url = urljoin(base_url, clicks_path)
+    bearer = "Bearer {t}".format(t=token)
+    headers = {
+        "Authorization": bearer 
+    }
+    params = {
+        "unit": "day",
+        "units": -1
+    }
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    clicks_json = response.json()['link_clicks']
+    clicks_count = clicks_json[0].get('clicks')
+    return clicks_count
+
 if __name__ == "__main__":
  link = input("Введите ссылку, которую нужно сократить:")
  try:  
     bitlink = shorten_link(link)
  except requests.exceptions.HTTPError:
-     print("Вы ввели не корректную ссылку")
-     sys.exit()   
- print('Битлинк', shorten_link(link))
+    print("Вы ввели не корректную ссылку")
+    sys.exit()   
+ print('Битлинк', bitlink)
